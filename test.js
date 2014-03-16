4654490917b98be41b2533fb8d2b300c6232bb07
Ellen Gummesson <ellengummesson@gmail.com>
2014-03-16 16:48:31 +0100

    Add more tests.

--- test.js
+++ test.js
@@ -2,19 +2,34 @@
 
 var assert = require('assert');
 var gutil  = require('gulp-util');
-var pixrem = require('./index');
+var pixrem = require('./');
 
 /* Tests */
 
-it('should postprocess CSS using Pixrem', function(done) {
-  var stream = pixrem();
+describe('gulp-pixrem', function() {
+  it('should postprocess CSS using Pixrem with the default pixel root value', function(done) {
+    var stream = pixrem();
 
-  stream.on('data', function(data) {
-    assert.equal(data.contents.toString(), 'code { font-size: 14px; font-size: 0.875rem; }');
-    done();
+    stream.on('data', function(data) {
+      assert.equal(data.contents.toString(), 'code { font-size: 14px; font-size: 0.875rem; }');
+      done();
+    });
+
+    stream.write(new gutil.File({
+      contents: new Buffer('code { font-size: 0.875rem; }')
+    }));
   });
 
-  stream.write(new gutil.File({
-    contents: new Buffer('code { font-size: 0.875rem; }')
-  }));
+  it('should postprocess CSS using Pixrem with a custom pixel root value', function(done) {
+    var stream = pixrem('10px');
+
+    stream.on('data', function(data) {
+      assert.equal(data.contents.toString(), 'code { font-size: 10px; font-size: 1rem; }');
+      done();
+    });
+
+    stream.write(new gutil.File({
+      contents: new Buffer('code { font-size: 1rem; }')
+    }));
+  });
 });
